18100625f62a5a005db2d5cc5b3f15707875163b
iamvirul <virulwickramasinghe@gmail.com>
2026-01-04 19:43:08 +0530

    Add ferrous-alloc library crate
    Create allocator library crate for kernel memory allocation.
    Allocator implementation will be added in Phase 1.
    
    Part of Phase 0 deliverables.

---
+++ lib/alloc/src/lib.rs
@@ -0,0 +1,15 @@
+//! Ferrous Allocator Library
+//!
+//! Memory allocation primitives for Ferrous Kernel.
+//!
+//! This library provides allocator implementations for kernel use.
+
+#![no_std]
+#![feature(allocator_api)]
+#![deny(unsafe_code)]
+#![warn(missing_docs)]
+
+extern crate alloc;
+
+// Allocator implementation will be added in Phase 1
+
